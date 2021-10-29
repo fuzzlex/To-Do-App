1e93d8676ba7780a64acb95c60521c02469a4d13
emre <emrky.2018@icloud.com>
2021-10-29 12:44:02 +0300

    update

--- script.js
+++ script.js
@@ -1,46 +1,55 @@
 const addEL = document.getElementById("add");
 const formEl = document.querySelector('form')
-const todos = document.querySelector('.todos')
 const newtodosDel = document.querySelector('.to-do-list_todos')
 
 
 
 addEL.addEventListener("click", ()=>{
            loader(); 
-               
+        
+        
+         
 }); 
 
 function loader() {
     let inputText = document.getElementById("inputText").value;
     let sontext = inputText.toUpperCase();
-    todos.innerHTML += 
-    `<div class="check">
+    newtodosDel.innerHTML += 
+    `<div  class="checkboxer"> 
     
-    <div  class="checkboxer"> 
-    
-    <input type="checkbox" class="fa-thumbs-up">
+    <i class="far fa-thumbs-up"></i>
     <label class="okey" >${sontext}</label> 
     <img class="delete" src="delete.svg" alt="">
-    </div>
-    
-    
-
 </div>`
 formEl.reset();
 }
 
+
+
+
+
 newtodosDel.addEventListener('click', (a) => {
     if (a.target.classList.contains('delete')) {
-      a.target.parentElement.remove();
+     a.target.parentElement.classList.toggle("changed2");
+   
        }
 
 });
 newtodosDel.addEventListener('click', (a) => {
     if (a.target.classList.contains('fa-thumbs-up')) {
-        a.target.nextElementSibling.classList.toggle("complated")     
+        a.target.nextElementSibling.classList.toggle("complated")
+        a.target.classList.toggle("changed1")
+     
        }
-});
 
+});
+newtodosDel.addEventListener('click', (a) => {
+    if (a.target.classList.contains('okey')) {
+        a.target.classList.toggle("complated")
+     
+       }
+
+});
 
 
 
